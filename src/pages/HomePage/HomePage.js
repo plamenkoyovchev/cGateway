92a2039ecdae06ddef12e2e0d3d1c0df84a947bd
plamenyovchev <plamenkooo@gmail.com>
2019-11-16 07:41:54 +0200

    Add app folder structure and pages

---
+++ src/pages/HomePage/HomePage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './HomePage.scss';
+
+const HomePage = () => {
+    return (
+        <h1 className='HomePage'>
+            Lisa Gateway Simulator Client
+        </h1>
+    );
+}
+
+export default HomePage;
