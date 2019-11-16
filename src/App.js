d9e65c3f628f6acef6e6a297e33f952e4c5b6018
plamenyovchev <plamenkooo@gmail.com>
2019-11-16 08:12:08 +0200

    Register home route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/HomePage/HomePage';
 
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      <Switch>
+        <Route path='/' component={HomePage} />
+      </Switch>
     </div>
   );
 }
