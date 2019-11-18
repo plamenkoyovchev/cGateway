b8b320cab6253a382f0e626fd27ccbb61ecc090b
Plamen Yovchev <plamenkooo@gmail.com>
2019-11-18 16:41:04 +0200

    Add Spinner while loading

--- src/components/LoadItems/LoadItems.jsx
+++ src/components/LoadItems/LoadItems.jsx
@@ -4,22 +4,31 @@ import axios from "../../http/axiosGateway";
 import "./LoadItems.scss";
 
 import LoadItem from "./LoadItem/LoadItem";
+import Spinner from "../UI/Spinner/Spinner";
 
 const LoadItems = () => {
   const [loads, setLoads] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     axios
       .get("/loads")
-      .then(response => setLoads(response.data.data))
-      .catch(err => {});
+      .then(response => {
+        setLoading(true);
+        setLoads(response.data.data);
+      })
+      .catch(err => {})
+      .finally(() => setLoading(false));
   }, []);
 
   return (
-    <div className="LoadItems">
-      {loads.map(({ id, ...otherProps }) => (
-        <LoadItem key={id} {...otherProps} />
-      ))}
+    <div>
+      {loading ? <Spinner /> : null}
+      <div className="LoadItems">
+        {loads.map(({ id, ...otherProps }) => (
+          <LoadItem key={id} {...otherProps} />
+        ))}
+      </div>
     </div>
   );
 };
