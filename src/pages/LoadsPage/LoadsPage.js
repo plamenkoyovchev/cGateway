ad83c0f97d9cc390366becf4cdd20c4b69ece03f
Plamen Yovchev <plamenkooo@gmail.com>
2019-11-18 14:53:17 +0200

    Get loads & add some styles

--- src/pages/LoadsPage/LoadsPage.js
+++ src/pages/LoadsPage/LoadsPage.js
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+
+import "./LoadsPage.scss";
+import axios from "../../http/axiosGateway";
 
 const LoadsPage = () => {
-    return (
-        <div>
-            Loads Page
+  const [loads, setLoads] = useState([]);
+  useEffect(() => {
+    axios
+      .get("/loads")
+      .then(response => setLoads(response.data.data))
+      .catch(err => console.log(err));
+  }, []);
+
+  return (
+    <div className="LoadsPage">
+      {loads.map(({ name, id, type, device }) => (
+        <div key={id} className="LoadItem card text-white bg-dark mb-3">
+          <div className="card-header">{id}</div>
+          <div className="card-body">
+            <h4 className="card-title">{name}</h4>
+            <p className="card-text">
+              {type} {device}
+            </p>
+          </div>
         </div>
-    );
-}
+      ))}
+    </div>
+  );
+};
 
 export default LoadsPage;
