1d496007c4174d0c4a591db6330248ece8abac21
plamenyovchev <plamenkooo@gmail.com>
2019-11-19 06:18:41 +0200

    Decrease LoadItem width

--- src/components/LoadItems/LoadItem/LoadItem.jsx
+++ src/components/LoadItems/LoadItem/LoadItem.jsx
@@ -4,15 +4,18 @@ import { Link } from "react-router-dom";
 
 import "./LoadItem.scss";
 
-const LoadItem = ({ id, name, type, device, match }) => {
+const LoadItem = ({ id, name, type, device, match, room }) => {
   return (
     <div className="LoadItem card">
       <div className="card-body">
         <h4 className="card-title">{name}</h4>
         <h6 className="card-subtitle mb-2 text-muted">{`Type: ${type}`}</h6>
-        <p className="card-text">{`Device ID: ${device}`}</p>
+        <div className="card-text">
+          <p className="card-subtitle">{`Device ID: ${device}`}</p>
+          <p>{`Room ID: ${room}`}</p>
+        </div>
         <Link
-          to={`${match.url}/${id}`}
+          to={{ pathname: `${match.url}/${id}` }}
           className="load-details-btn btn btn-primary"
         >
           Details
