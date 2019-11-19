cc173a85c112f429ca7beddaa2e35ff9844db440
plamenyovchev <plamenkooo@gmail.com>
2019-11-19 06:19:02 +0200

    Add Route for LoadItemDetails

--- src/App.js
+++ src/App.js
@@ -10,12 +10,15 @@ import RoomsPage from './pages/RoomsPage/RoomsPage';
 import DevicesPage from './pages/DevicesPage/DevicesPage';
 import MomentsPage from './pages/MomentsPage/MomentsPage';
 
-function App() {
+import LoadItemDetails from './components/LoadItems/LoadItemDetails/LoadItemDetails';
+
+const App = props => {
   return (
     <div className="App">
       <Layout>
         <Switch>
-          <Route path='/loads' component={LoadsPage} />
+          <Route exact path='/loads' component={LoadsPage} />
+          <Route path='/loads/:loadId' component={LoadItemDetails} />
           <Route path='/rooms' component={RoomsPage} />
           <Route path='/devices' component={DevicesPage} />
           <Route path='/moments' component={MomentsPage} />
