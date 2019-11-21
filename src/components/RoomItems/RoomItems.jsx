d90f44bd2f8393bffba16efdd21c363190ca8103
Plamen Yovchev <plamenkooo@gmail.com>
2019-11-21 12:55:39 +0200

    Add Spinner component to RoomItems

--- src/components/RoomItems/RoomItems.jsx
+++ src/components/RoomItems/RoomItems.jsx
@@ -4,20 +4,24 @@ import "./RoomItems.scss";
 
 import axios from "../../http/axiosGateway";
 import RoomItem from "./RoomItem/RoomItem";
+import Spinner from "../UI/Spinner/Spinner";
 
 const RoomItems = () => {
   const [rooms, setRooms] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
+    setLoading(true);
     axios
       .get("/rooms")
       .then(response => setRooms(response.data.data))
       .catch(err => {})
-      .finally();
+      .finally(() => setLoading(false));
   }, []);
 
   return (
     <div className="RoomItems">
+      {loading ? <Spinner /> : null}
       <ul className="list-group">
         {rooms.map(({ id, ...otherProps }) => (
           <RoomItem key={id} id={id} {...otherProps} />
